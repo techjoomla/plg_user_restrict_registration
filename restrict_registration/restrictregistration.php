7a7ff689d27c94683374827e832739533d9b70d7
Megha Biranje <megha_b@techjoomla.com>
2021-11-26 00:30:06 +0530

    Add active login restrict code

--- restrict_registration/restrictregistration.php
+++ restrict_registration/restrictregistration.php
@@ -11,6 +11,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\User\User;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Session\Session;
 use Joomla\CMS\User\UserHelper;
@@ -102,7 +103,8 @@ class PlgUserRestrictRegistration extends CMSPlugin
 		$app = Factory::getApplication();
 		$now = new \DateTime();
 
-		$loginRestrictPermission = $this->params->get('login_restrict');
+		$loginRestrictPermission       = $this->params->get('login_restrict');
+		$activeLoginRestrictPermission = $this->params->get('active_login_restrict');
 
 		if ($loginRestrictPermission)
 		{
@@ -164,6 +166,130 @@ class PlgUserRestrictRegistration extends CMSPlugin
 			}
 		}
 
+		if ($activeLoginRestrictPermission)
+		{
+			$activeLoginUserCount = $this->params->get('max_active_logins');
+
+			if ($this->app->isClient('site'))
+			{
+				$instance = $this->_getUser($user, $options);
+
+				// If _getUser returned an error, then pass it back.
+				if ($instance instanceof Exception)
+				{
+					return false;
+				}
+
+				// If the user is blocked, redirect with an error
+				if ($instance->block == 1)
+				{
+					$this->app->enqueueMessage(Text::_('JERROR_NOLOGIN_BLOCKED'), 'warning');
+
+					return false;
+				}
+
+				// Check the user can login.
+				$result = $instance->authorise($options['action']);
+
+				if (!$result)
+				{
+					$this->app->enqueueMessage(Text::_('JERROR_LOGIN_DENIED'), 'warning');
+
+					return false;
+				}
+
+				// Get the user sessions
+				$this->db = JFactory::getDbo();
+
+				$query = $this->db->getQuery(true);
+				$query->select('b.*');
+				$query->from($this->db->quoteName('#__session', 'b'));
+				$query->where($this->db->quoteName('b.guest') . ' = 0');
+				$query->where('(' . $this->db->quoteName('b.client_id') . ' = 0' . ' OR ' . $this->db->quoteName('b.client_id') . ' IS NULL' . ')'); // 0 is for users, 1 is for admins
+				$query->where($this->db->quoteName('b.userid') . ' = '. (int) $instance->id);
+
+				try
+				{
+					$this->db->setQuery($query);
+					$count_of_user_sessions = count($this->db->loadObjectlist());
+				}
+				catch (RuntimeException $e)
+				{
+					JError::raiseError(500, $e->getMessage());
+					return false;
+				}
+
+				if ($count_of_user_sessions >= $activeLoginUserCount)
+				{
+					$this->app->enqueueMessage(Text::sprintf('PLG_USER_RESTRICT_ACTIVE_LOGIN_ERROR_MESSAGE', $activeLoginUserCount), 'warning');
+					Factory::getApplication()->setUserState('com_users.action.uid', (int) $userId);
+					$redirect_to_url = URI::root().'index.php?option=com_users&view=login&'.Session::getFormToken().'=1';
+					Factory::getApplication()->redirect($redirect_to_url);
+
+					return false;
+				}
+			}
+		}
+
 		return true;
+	}
+
+	/**
+	 * This method will return a user object
+	 *
+	 * If options['autoregister'] is true, if the user doesn't exist yet they will be created
+	 *
+	 * @param   array  $user     Holds the user data.
+	 * @param   array  $options  Array holding options (remember, autoregister, group).
+	 *
+	 * @return  User
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function _getUser($user, $options = array())
+	{
+		$instance = User::getInstance();
+		$id = (int) UserHelper::getUserId($user['username']);
+
+		if ($id)
+		{
+			$instance->load($id);
+
+			return $instance;
+		}
+
+		// TODO : move this out of the plugin
+		$params = ComponentHelper::getComponent('com_users')->getParams();
+
+		// Read the default user group option from com_users
+		$defaultUserGroup = $params->get('new_usertype', $params->get('guest_usergroup', 1));
+
+		$instance->id = 0;
+		$instance->name = $user['fullname'];
+		$instance->username = $user['username'];
+		$instance->password_clear = $user['password_clear'];
+
+		// Result should contain an email (check).
+		$instance->email = $user['email'];
+		$instance->groups = array($defaultUserGroup);
+
+		// If autoregister is set let's register the user
+		$autoregister = isset($options['autoregister']) ? $options['autoregister'] : $this->params->get('autoregister', 1);
+
+		if ($autoregister)
+		{
+			if (!$instance->save())
+			{
+				$this->app->enqueueMessage(Text::_('Error in autoregistration for user ' . $user['username'] . '.'), 'error');
+				return false;
+			}
+		}
+		else
+		{
+			// No existing user and autoregister off, this is a temporary user.
+			$instance->set('tmp_user', true);
+		}
+
+		return $instance;
 	}
 }
